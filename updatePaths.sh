dd89ddcd7c4687bfff789015bf8e3b1e422cbb39
piet <pprotassow@posteo.de>
2022-06-28 19:38:45 +0200

    feat: updates new paths for tmux-session

--- updatePaths.sh
+++ updatePaths.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
 source "${HOME}/personal/productivity/bashqtools/arrayMethods.sh" # get join
-source "${HOME}/personal/productivity/paths.sh" #paths
+FILE="${HOME}/personal/productivity/paths.sh" #paths
+source "${FILE}"
 
 echo "${paths[@]}"
-
-newPaths=$(append paths bla)
+newPaths=$(append paths "$(pwd)")
 echo "${newPaths}"
+
+echo "updating ${FILE}"
+echo -en '\n' >> $FILE 
+echo "paths=(${newPaths})" >> $FILE 
