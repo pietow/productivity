27e95f12f25d517726d061aad2919413019835e6
piet <pprotassow@posteo.de>
2022-07-13 00:04:07 +0200

    feat: paths of tmux-session

--- paths.sh
+++ paths.sh
@@ -1,2 +1,6 @@
 paths=(~/personal/ ~/.config/nvim/ ~/DCI/ ~/DCI/fullstack_submodule/ ~/DCI/fullstack_submodule/server-express/)
 
+
+paths=(/home/dci-student/personal/ /home/dci-student/.config/nvim/ /home/dci-student/DCI/ /home/dci-student/DCI/fullstack_submodule/ /home/dci-student/DCI/fullstack_submodule/server-express/ /home/dci-student/MyProjects)
+
+paths=(/home/dci-student/personal/ /home/dci-student/.config/nvim/ /home/dci-student/DCI/ /home/dci-student/DCI/fullstack_submodule/ /home/dci-student/DCI/fullstack_submodule/server-express/ /home/dci-student/MyProjects /home/dci-student/MyProjects/quantra)
