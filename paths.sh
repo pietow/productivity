d0561256be008a9f07863130b332f40c0c63dc0c
piet <pprotassow@posteo.de>
2022-07-15 01:43:27 +0200

    path added

--- paths.sh
+++ paths.sh
@@ -4,3 +4,5 @@ paths=(~/personal/ ~/.config/nvim/ ~/DCI/ ~/DCI/fullstack_submodule/ ~/DCI/fulls
 paths=(/home/dci-student/personal/ /home/dci-student/.config/nvim/ /home/dci-student/DCI/ /home/dci-student/DCI/fullstack_submodule/ /home/dci-student/DCI/fullstack_submodule/server-express/ /home/dci-student/MyProjects)
 
 paths=(/home/dci-student/personal/ /home/dci-student/.config/nvim/ /home/dci-student/DCI/ /home/dci-student/DCI/fullstack_submodule/ /home/dci-student/DCI/fullstack_submodule/server-express/ /home/dci-student/MyProjects /home/dci-student/MyProjects/quantra)
+
+paths=(/home/dci-student/personal/ /home/dci-student/.config/nvim/ /home/dci-student/DCI/ /home/dci-student/DCI/fullstack_submodule/ /home/dci-student/DCI/fullstack_submodule/server-express/ /home/dci-student/MyProjects /home/dci-student/MyProjects/quantra /home/dci-student)
