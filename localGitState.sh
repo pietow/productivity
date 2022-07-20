7a1f383fbedaf9ca48cde2a6e6655fac85fbd5fc
piet <pprotassow@posteo.de>
2022-07-21 00:04:15 +0200

    clean + get all relevant repos

---
+++ localGitState.sh
@@ -0,0 +1,62 @@
+git clone git@github.com:Fbw-wd22-e01-Rockstars/03-browser-dom-manipulating-pietow.git /home/dci-student/.local/share/Trash/files/03-browser-dom-manipulating-pietow
+
+git clone git@github.com:pietow/.dotfiles.git /home/dci-student/personal/.dotfiles
+
+git clone git@github.com:pietow/neovim-init.vim.git /home/dci-student/.config/nvim
+
+git clone git@github.com:Fbw-wd22-e01-Rockstars/08-browser-passwordgenerator-pietow.git /home/dci-student/DCI/08-browser-passwordgenerator-pietow
+
+git clone git@github.com:pietow/eslintPrettier.git /home/dci-student/DCI/eslintPrettier
+
+git clone git@github.com:Fbw-wd22-e01-Rockstars/04-spa-dom-traversing-pietow.git /home/dci-student/DCI/04-spa-dom-traversing-pietow
+
+git clone git@github.com:Fbw-wd22-e01-Rockstars/05-browser-dom-traversing-parents-list-pietow.git /home/dci-student/DCI/05-browser-dom-traversing-parents-list-pietow
+
+git clone git@github.com:Fbw-wd22-e01-Rockstars/pb-shrugman-pietow.git /home/dci-student/DCI/pb-shrugman-pietow
+
+git clone git@github.com:Fbw-wd22-e01-Rockstars/14-browser-modules-imports-exports-pietow.git /home/dci-student/DCI/14-browser-modules-imports-exports-pietow
+
+git clone git@github.com:Fbw-wd22-e01-Rockstars/01-browser-dom-window-pietow.git /home/dci-student/DCI/01-browser-dom-window-pietow
+
+git clone git@github.com:pietow/linear_regression.git /home/dci-student/DCI/linear_regression
+
+git clone git@github.com:Fbw-wd22-e01-Rockstars/pb-bulls-and-cows-pietow.git /home/dci-student/DCI/pb-bulls-and-cows-pietow
+
+git clone git@github.com:Fbw-wd22-e01-Rockstars/07-browser-calculator-pietow.git /home/dci-student/DCI/07-browser-calculator-pietow
+
+git clone git@github.com:anudesi/11-07-2022.git /home/dci-student/DCI/11-07-2022
+
+git clone git@github.com:Fbw-wd22-e01-Rockstars/16-browser-fetch-promises-pietow.git /home/dci-student/DCI/16-browser-fetch-promises-pietow
+
+git clone git@github.com:FbW-E05-1/lessons.git /home/dci-student/DCI/lessons
+
+git clone git@github.com:Fbw-wd22-e01-Rockstars/11-browser-registrationform-pietow.git /home/dci-student/DCI/11-browser-registrationform-pietow
+
+git clone git@github.com:pietow/mandelbrotreact.git /home/dci-student/DCI/frontend/mandelbrotreact
+
+git clone git@github.com:Fbw-wd22-e01-Rockstars/03-browser-dom-manipulating-pietow.git /home/dci-student/DCI/03-browser-dom-manipulating-pietow
+
+git clone git@github.com:Fbw-wd22-e01-Rockstars/15-browser-comingsoon-pietow.git /home/dci-student/DCI/15-browser-comingsoon-pietow
+
+git clone git@github.com:Charlibb/client_to_do.git /home/dci-student/DCI/client_to_do
+
+git clone git@github.com:pietow/fullstack_submodule.git /home/dci-student/DCI/fullstack_submodule
+
+git clone git@github.com:Fbw-wd22-e01-Rockstars/02-spa-boilerplate-rendering-dynamic-data-pietow.git /home/dci-student/DCI/02-spa-boilerplate-rendering-dynamic-data-pietow
+
+git clone git@github.com:DCI-TrialLessons/TL-AT-UIB-box-model.git /home/dci-student/DCI/TRIAL_LESSON/TL-AT-UIB-box-model
+
+git clone git@github.com:Fbw-wd22-e01-Rockstars/13-browser-modules-basics-pietow.git /home/dci-student/DCI/13-browser-modules-basics-pietow
+
+git clone git@github.com:D04-2/DCI-Course.git /home/dci-student/DCI/D04-2/DCI-Course
+
+git clone git@github.com:Fbw-wd22-e01-Rockstars/02-browser-dom-querying-pietow.git /home/dci-student/DCI/02-browser-dom-querying-pietow
+
+git clone git@github.com:Fbw-wd22-e01-Rockstars/06-browser-weekendcountdown-pietow.git /home/dci-student/DCI/06-browser-weekendcountdown-pietow
+
+git clone git@github.com:Fbw-wd22-e01-Rockstars/03-spa-boilerplate-building-layout-pietow.git /home/dci-student/DCI/03-spa-boilerplate-building-layout-pietow
+
+git clone git@github.com:heyitsashleyhere/ICT-SPA-TeamProject.git /home/dci-student/DCI/ICT-SPA-TeamProject
+
+git clone git@github.com:Fbw-wd22-e01-Rockstars/01-spa-boilerplate-jsx-pietow.git /home/dci-student/DCI/01-spa-boilerplate-jsx-pietow
+
--- traverseGit.sh
+++ traverseGit.sh
@@ -1,9 +1,19 @@
+#!/usr/bin/env bash
+
 dirlist=$(find ${HOME}  -type d | grep -v '.git/' | grep '/.git$')
+
+SUB='git@github.com'
+
+# URL="$(git remote get-url origin)"
+# echo "git clone" "$URL" "$(pwd)" >> out.txt
 
 for dir in $dirlist
 do
   (
   cd $dir
-  echo "git clone" "$(git remote get-url --all origin)" "$(pwd)" 
-  )
-done
+  URL="$(git remote get-url origin)"
+  if [[ "$URL" == *"$SUB"* ]]; then
+      echo -e "git clone" "$URL" "$(pwd | sed 's/\/.git//')\n" 
+  fi
+  );
+done >> localGitState.sh
